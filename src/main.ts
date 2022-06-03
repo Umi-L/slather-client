7a07038d18e2c1575226006890262c16b7f003b7
Umi <julianogrady07@gmail.com>
2022-06-02 22:53:05 -0400

    food

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,13 @@ interface PlayerData{
   speed: number;
 }
 
+interface FoodOrb{
+  x: number;
+  y: number;
+  value: number;
+  color: string;
+}
+
 const fps = 60;
 
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
@@ -30,12 +37,17 @@ let id:number;
 
 
 let snakeRadius = 10;
+let speed = 0;
 
+let mapHeight = 0;
+let mapWidth = 0;
 
 let posX = 0;
 let posY = 0;
 let body : SnakePoint[] = [];
 let heading = 0;
+
+let foodOrbs:FoodOrb[] = [];
 
 document.getElementById("login-button")!.onclick = joinGame;
 
@@ -73,8 +85,18 @@ function joinGame(){
         let jsonData = JSON.parse(ev.data);
 
         switch(jsonData.method){
-          case "sendSelf":
-            id = jsonData.id;
+          case "initialData":
+            id = jsonData.data.id;
+
+            mapWidth = jsonData.data.mapWidth;
+            mapHeight = jsonData.data.mapHeight;
+
+            speed = jsonData.data.speed;
+
+            foodOrbs = jsonData.data.orbs;
+            console.log(foodOrbs);
+          
+
             document.getElementById("login")?.setAttribute("style", "visibility: hidden;")
             break;
 
@@ -85,6 +107,14 @@ function joinGame(){
             posY = users[id].body[0].y;
 
             body = users[id].body;
+
+            foodOrbs = foodOrbs.concat(jsonData.orbs);
+
+            foodOrbs.forEach((orb)=>{
+              if (!orb.color){
+                orb.color = `rgb(${Math.random()*255}, ${Math.random()*255}, ${Math.random()*255})`;
+              }
+            })
 
             break;
         }
@@ -105,6 +135,9 @@ function joinGame(){
 }
 
 function drawSnakes(){
+  ctx.fillStyle = "#32a852";
+  ctx.strokeStyle = "#000000";
+
   Object.keys(users).forEach((userID:any) =>{
 
     if (userID == id)
@@ -118,6 +151,7 @@ function drawSnakes(){
       //draw circle
       ctx.beginPath();
       ctx.arc(segment.x - offsetX, segment.y - offsetY, snakeRadius, 0, 2 * Math.PI)
+      ctx.fill();
       ctx.stroke();
     })
   })
@@ -127,11 +161,16 @@ function drawSelf(){
   let offsetX = posX - canvas.width / 2;
   let offsetY = posY - canvas.height / 2;
 
+  ctx.fillStyle = "#32a852";
+  ctx.strokeStyle = "#000000";
+
+
   body.forEach((segment)=>{
 
     //draw circle
     ctx.beginPath();
     ctx.arc(segment.x - offsetX, segment.y - offsetY, snakeRadius, 0, 2 * Math.PI)
+    ctx.fill();
     ctx.stroke();
   })
 }
@@ -142,8 +181,17 @@ function clearCanvas(){
 
 function nextFrame(){
   clearCanvas();
+
+  //updateSnakePos();
+
   drawSelf();
   drawSnakes();
+  drawBorder();
+  
+  checkFoodOrbs();
+
+  drawOrbs();
+
 }
 
 function updateServer(){
@@ -153,11 +201,74 @@ function updateServer(){
 
   socket.send(JSON.stringify({method:"update", data: _data}))
 }
+function drawBorder(){
+  let offsetX = posX - canvas.width / 2;
+  let offsetY = posY - canvas.height / 2;
+
+  ctx.rect(0 - offsetX - snakeRadius, 0 - offsetY - snakeRadius, mapWidth + snakeRadius*2, mapHeight + snakeRadius*2);
+  ctx.stroke();
+}
 
 function angleBetween(x1:number, y1:number, x2:number, y2:number){
   return Math.atan2( y2 - y1, x2 - x1 );
 }
 
+function updateSnakePos(){
+  //move snake forward
+
+  let dt = 20 / 1000;
+
+  let previousPoint = body[0];
+
+  let nextPoint = {
+      x: speed * dt * Math.cos(heading) + previousPoint.x,
+      y: speed * dt * Math.sin(heading) + previousPoint.y,
+  } as SnakePoint;
+
+
+  nextPoint = clampPoint(nextPoint);
+
+  body.unshift(nextPoint);
+  body.pop();
+}
+
+function drawOrbs(){
+  let offsetX = posX - canvas.width / 2;
+  let offsetY = posY - canvas.height / 2;
+
+  foodOrbs.forEach((orb)=>{
+
+    ctx.fillStyle = orb.color;
+
+    ctx.beginPath();
+    ctx.arc(orb.x - offsetX, orb.y - offsetY, orb.value * 70 + 10, 0, 2 * Math.PI)
+    ctx.fill();
+    ctx.stroke();
+  })
+}
+
 document.addEventListener("mousemove", (ev) =>{  
   heading = angleBetween(canvas.width/2, canvas.height/2, ev.x, ev.y);
 })
+
+function clamp(num:number, min:number, max:number) {
+  return Math.min(Math.max(num, min), max)
+};
+
+function clampPoint(point:SnakePoint): SnakePoint{
+  return {x: clamp(point.x, 0, mapWidth), y: clamp(point.y, 0, mapHeight)}
+}
+
+function checkFoodOrbs(){
+  for (let i = foodOrbs.length - 1; i >= 0; i--)
+  {
+      if (pointDist(foodOrbs[i].x, foodOrbs[i].y, posX, posY) < 20){
+          foodOrbs.splice(i, 1);
+      }
+  }       
+}
+
+
+function pointDist(x1:number, y1:number, x2:number, y2:number){
+  return Math.abs(Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2)))
+}
